736165915d993fbcc0e39e81a1792d87ba244f30
Svetlana <khokhlachevass@gmail.com>
2024-03-28 00:00:26 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,84 @@
+import manager.TaskManager;
+import tasks.Epic;
+import tasks.Subtask;
+import tasks.Task;
+import tasks.Status;
+
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Поехали!");
+
+        TaskManager manager = new TaskManager();
+
+        System.out.println("Задачи");
+        System.out.println("Создать задачи");
+        manager.createTask(new Task("Сдать модуль по Java до 29.03.2024", "Task-1", Status.NEW));
+        manager.createTask(new Task("Оправить письмо бабушке", "Task-2", Status.NEW));
+        manager.printTasks();
+        System.out.println("Показать задания");
+        List<Task> taskList = manager.getAllTasks();
+        System.out.println(taskList);
+        System.out.println("Присвоить задаче номер");
+        Task task = manager.getTaskById(1);
+        System.out.println(task);
+        System.out.println("Обновить задачу");
+        task.setStatus(Status.IN_PROGRESS);
+        manager.updateTask(task);
+        System.out.println(task);
+        System.out.println();
+
+        System.out.println("Epic");
+        manager.createEpic(new Epic("Сдать работу по 4 спринту", "Epic-1", Status.NEW));
+        manager.createEpic(new Epic("Распечатать фотографии", "Epic-2", Status.NEW));
+        manager.printEpics();
+        List<Epic> epics = manager.getAllEpics();
+        System.out.println(epics);
+        Epic epic = manager.getEpicById(3);
+        System.out.println(epic);
+        epic.setStatus(Status.IN_PROGRESS);
+        manager.updateEpic(epic);
+        Epic epic3 = manager.getEpicById(3);
+        System.out.println(epic3);
+        System.out.println();
+
+        System.out.println("*** Subtask ***");
+        manager.createSubtask(new Subtask("Пить стакан теплой воды на тощак", "Subtask-1", Status.NEW, 3));
+        manager.createSubtask(new Subtask("Не устраивать ночные дожоры", "Subtask-2", Status.NEW, 3));
+        manager.createSubtask(new Subtask("Начать спать хотябы 6 часов в сутки", "Subtask-3", Status.NEW, 4));
+        manager.createSubtask(new Subtask("Больше бывать на свежем воздухе", "Subtask-4", Status.NEW, 4));
+        manager.printSubtasks();
+        List<Subtask> subtasksByEpicId = manager.getAllSubtasksByEpicId(3);
+        System.out.println(subtasksByEpicId);
+        List<Subtask> subtasks = manager.getAllSubtasks();
+        System.out.println(subtasks);
+        Subtask subtask = manager.getSubtaskById(5);
+        System.out.println(subtask);
+        subtask.setStatus(Status.IN_PROGRESS);
+        manager.updateSubtask(subtask);
+        System.out.println(subtask);
+        System.out.println();
+
+        System.out.println("Выполняем задачи");
+        System.out.println("Выполнено");
+        manager.deleteTaskById(1);
+        System.out.println(taskList);
+        System.out.println("Выполнено");
+        manager.deleteAllTasks();
+        manager.printTasks();
+        System.out.println("Выполнено");
+        manager.deleteSubtaskById(5);
+        manager.printSubtasks();
+        System.out.println("Выполнено");
+        manager.deleteAllSubtasks();
+        manager.printSubtasks();
+        System.out.println("Выполнено");
+        manager.deleteEpicById(4);
+        manager.printEpics();
+        System.out.println("Выполнено");
+        manager.deleteAllEpics();
+        manager.printEpics();
     }
 }
