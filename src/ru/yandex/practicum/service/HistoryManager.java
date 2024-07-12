bce5ed9bc27b8887c2b8120db6bac37eb1c2e2d9
Svetlana <khokhlachevass@gmail.com>
2024-07-13 00:44:37 +0300

    sprint6

---
+++ src/ru/yandex/practicum/service/HistoryManager.java
@@ -0,0 +1,12 @@
+package ru.yandex.practicum.service;
+import ru.yandex.practicum.model.Task;
+
+import java.util.List;
+
+public interface HistoryManager {
+    void add(Task task);
+
+    void remove(int id);
+
+    List<Task> getHistory();
+}
---
+++ src/test/ManagerTest.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.service.Managers;
+
+@DisplayName("Менеджеры")
+public class ManagerTest {
+
+    @Test
+    @DisplayName("Программа должна корректно собираться")
+    public void shouldCorrectlyAssembleProgramm() {
+        Assertions.assertNotNull(Managers.getDefault());
+        Assertions.assertNotNull(Managers.getDefaultHistory());
+    }
+}
