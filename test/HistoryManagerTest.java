dc08b31fb28d8d99d0185935fe0f43972bd84ce0
Svetlana <khokhlachevass@gmail.com>
2024-07-15 23:39:38 +0300

    popitka618

--- test/HistoryManagerTest.java
+++ test/HistoryManagerTest.java
@@ -13,8 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HistoryManagerTest {
 
-
-
     @Test
     void checkSizeOfRequestHistory() {
         HistoryManager historyManager = Managers.getDefaultHistory();
