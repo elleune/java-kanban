4471fb60c1976b68abc1f3d93f8e980754c59d33
Svetlana <khokhlachevass@gmail.com>
2024-07-15 22:50:12 +0300

    popitka617

--- test/HistoryManagerTest.java
+++ test/HistoryManagerTest.java
@@ -13,12 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HistoryManagerTest {
 
-    @Test
-    void newHistoryManager() {
-        HistoryManager historyManager = Managers.getDefaultHistory();
 
-        assertNotNull(historyManager, "Менеджер не проинициализирован");
-    }
 
     @Test
     void checkSizeOfRequestHistory() {
@@ -26,6 +21,7 @@ public class HistoryManagerTest {
         Task task = new Task();
         final int sizeFromRequestHistoryShouldBe = 1;
         final int sizeForCheckRequestSize = 10;
+
         for (int i = 0; i <= sizeForCheckRequestSize; i++) {
             historyManager.add(task);
         }
@@ -40,6 +36,7 @@ public class HistoryManagerTest {
         HistoryManager historyManager = Managers.getDefaultHistory();
         Task task = new Task();
         historyManager.add(task);
+
         final List<Task> history = historyManager.getHistory();
         assertNotNull(history, "История не пустая.");
         assertEquals(1, history.size(), "История не пустая.");
