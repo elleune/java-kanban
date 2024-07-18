d04178b9bd8342f71bbb7ab48a2c4b1cd2d3b371
elleune <156022042+elleune@users.noreply.github.com>
2024-07-18 22:03:31 +0300

    Update InMemoryHistoryManager.java

--- src/ru/yandex/practicum/service/InMemoryHistoryManager.java
+++ src/ru/yandex/practicum/service/InMemoryHistoryManager.java
@@ -13,6 +13,7 @@ public class InMemoryHistoryManager implements HistoryManager {
     private Node head;
     private Node tail;
 // переписан почти полностью класс InMemoryHistoryManager;
+    
     @Override
     public void add(Task task) {
         if (task == null) {
