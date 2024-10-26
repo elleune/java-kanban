2ba9a9a0c2499854b8b8c260cb6021451ce70828
elleune <156022042+elleune@users.noreply.github.com>
2024-10-26 16:08:45 +0300

    Update EpicHandler.java

--- src/ru/yandex/practicum/http/handlers/EpicHandler.java
+++ src/ru/yandex/practicum/http/handlers/EpicHandler.java
@@ -30,6 +30,7 @@ public class EpicHandler extends BaseHttpHandler implements HttpHandler {
                 .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
                 .create();
     }
+
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         switch (exchange.getRequestMethod()) {
@@ -46,55 +47,56 @@ public class EpicHandler extends BaseHttpHandler implements HttpHandler {
                 sendNotFound(exchange, "Не найден");
         }
     }
-        private void handleGetEpic(HttpExchange exchange) throws IOException {
-            String query = exchange.getRequestURI().getQuery();
-            if (query != null && query.startsWith("id=")) {
-                int id = Integer.parseInt(query.substring(3));
-                Epic epic = taskManager.getEpicById(id);
-                if (epic != null) {
-                    String response = gson.toJson(epic);
-                    sendText(exchange, response, 200);
-                } else {
-                    sendNotFound(exchange, "Не найден");
-                }
-            } else {
-                List<Epic> epics = taskManager.getAllEpics();
-                String response = gson.toJson(epics);
+
+    private void handleGetEpic(HttpExchange exchange) throws IOException {
+        String query = exchange.getRequestURI().getQuery();
+        if (query != null && query.startsWith("id=")) {
+            int id = Integer.parseInt(query.substring(3));
+            Epic epic = taskManager.getEpicById(id);
+            if (epic != null) {
+                String response = gson.toJson(epic);
                 sendText(exchange, response, 200);
-            }
-        }
-
-        private void handlePostEpic(HttpExchange exchange) throws IOException {
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody(),
-                    StandardCharsets.UTF_8))) {
-                String json = reader.lines().collect(Collectors.joining());
-                Epic epic = gson.fromJson(json, Epic.class);
-
-                if (epic.getId() > 0) {
-                    taskManager.updateEpic(epic);
-                    sendText(exchange, gson.toJson(epic), 200);
-                } else {
-                    int id = taskManager.createEpic(epic);
-                    sendText(exchange, gson.toJson(epic), 201);
-                }
-            } catch (Exception e) {
-                sendText(exchange, "Внутренняя ошибка сервера", 500);
-            }
-        }
-
-        private void handleDeleteEpic(HttpExchange exchange) throws IOException {
-            String query = exchange.getRequestURI().getQuery();
-            if (query != null && query.startsWith("id=")) {
-                int id = Integer.parseInt(query.substring(3));
-                Epic epic = taskManager.getEpicById(id);
-                if (epic != null) {
-                    taskManager.deleteEpicById(id);
-                    sendText(exchange, "Epic с id " + id + " удален", 200);
-                } else {
-                    sendText(exchange, "Epic не найден", 404);
-                }
             } else {
                 sendNotFound(exchange, "Не найден");
             }
+        } else {
+            List<Epic> epics = taskManager.getAllEpics();
+            String response = gson.toJson(epics);
+            sendText(exchange, response, 200);
         }
     }
+
+    private void handlePostEpic(HttpExchange exchange) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody(),
+                StandardCharsets.UTF_8))) {
+            String json = reader.lines().collect(Collectors.joining());
+            Epic epic = gson.fromJson(json, Epic.class);
+
+            if (epic.getId() > 0) {
+                taskManager.updateEpic(epic);
+                sendText(exchange, gson.toJson(epic), 200);
+            } else {
+                int id = taskManager.createEpic(epic);
+                sendText(exchange, gson.toJson(epic), 201);
+            }
+        } catch (Exception e) {
+            sendText(exchange, "Внутренняя ошибка сервера", 500);
+        }
+    }
+
+    private void handleDeleteEpic(HttpExchange exchange) throws IOException {
+        String query = exchange.getRequestURI().getQuery();
+        if (query != null && query.startsWith("id=")) {
+            int id = Integer.parseInt(query.substring(3));
+            Epic epic = taskManager.getEpicById(id);
+            if (epic != null) {
+                taskManager.deleteEpicById(id);
+                sendText(exchange, "Epic с id " + id + " удален", 200);
+            } else {
+                sendText(exchange, "Epic не найден", 404);
+            }
+        } else {
+            sendNotFound(exchange, "Не найден");
+        }
+    }
+}
